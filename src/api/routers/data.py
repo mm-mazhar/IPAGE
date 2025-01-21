7c064bac3827b3e5f9811403c76c4f255a0a5ef0
Mazhar <mazqoty.01@gmail.com>
2025-01-22 00:17:52 +0300

    Update

--- src/api/routers/data.py
+++ src/api/routers/data.py
@@ -1,4 +1,5 @@
-from fastapi import APIRouter, HTTPException, JSONResponse, status
+from fastapi import APIRouter, HTTPException, Response, status
+from fastapi.responses import JSONResponse
 
 from src.api import utils
 from src.api.db import DB
@@ -48,16 +49,48 @@ def get_data(limit: int = None):
 
 
 @data_router.post("/create-data")
-def create_data(data: RawData):
+def create_data(data: RawData, response: Response):
+    """
+    Create a new record in the database
+    EXAMPLE:
+        POST /data/create-data
+        {
+            "Area": "Area",
+            "pH": "pH",
+            "Nitrogen": "Nitrogen",
+            "Phosphorus": "Phosphorus",
+            "Potassium": "Potassium",
+            "Sulfur": "Sulfur",
+            "Zinc": "Zinc",
+            ...
+        }
+    Args:
+        data (RawData): The data to be stored in the database
+    Response:
+        fastapi.Response: The response object with status code 409 if the data already exists
+        JSONResponse:
+            A JSON response containing the status of the operation and the data stored
+        HTTPException:
+            An exception raised when an error occurs
+    """
     duplicated = False
     status = "Failed"
-    data = db.create_soil_data(data.dict())
+
+    try:
+        data = db.create_soil_data(data.model_dump())
+    except Exception as e:
+        return HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=str(e)
+        )
 
     if isinstance(data, SoilData):
         status = "Success"
         data = utils.object_to_dict(data)
     elif data is None:
+        response.status_code = status.HTTP_409_CONFLICT
         duplicated = True
+        status = "Data already exists in the database"
+
     db.end_session()
     return {"status": status, "duplicated": duplicated, "data": data}
 
