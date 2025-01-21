8245f576ff8eb1899f030a8821fda343e5787b5c
Mazhar <mazqoty.01@gmail.com>
2025-01-21 23:30:34 +0300

    Utility Files Added

---
+++ src/api/utils/__init__.py
@@ -0,0 +1,1 @@
+from .utils import object_to_dict
---
+++ src/api/utils/utils.py
@@ -0,0 +1,7 @@
+from sqlalchemy.orm import class_mapper
+
+
+def object_to_dict(obj):
+    # Extract columns from the SQLAlchemy model
+    columns = [c.key for c in class_mapper(obj.__class__).columns]
+    return {column: getattr(obj, column) for column in columns}
