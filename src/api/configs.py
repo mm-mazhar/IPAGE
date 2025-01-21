f76368ec0c6f0819b60e208aa229f77545394c42
Mazhar <mazqoty.01@gmail.com>
2025-01-22 01:15:43 +0300

    Update URL for API

--- src/api/configs.py
+++ src/api/configs.py
@@ -44,12 +44,12 @@ class Settings(BaseSettings):
 
     SERVERS: list[dict[str, str]] = [
         {
-            "url": "https://more-socially-fly.ngrok-free.app",
-            "description": "NGROK server with HTTPS",
+            "url": "https://ipage.onrender.com",
+            "description": "Render server with HTTPS",
         },
         # {
-        #     "url": "https://ipage-api.onrender.com",
-        #     "description": "Render server with HTTPS",
+        #     "url": "https://more-socially-fly.ngrok-free.app",
+        #     "description": "NGROK server with HTTPS",
         # },
         # {
         #     "url": "http://localhost:8080",
